298ec454002086ac309b58e951813cde0e9d90ed
isam14 <imuck14@gmail.com>
2017-07-10 16:46:36 +0200

    Ajout du HTML modifié avec js et css

--- app.js
+++ app.js
@@ -1,21 +1,54 @@
 alert('Bienvenue');
 
-// TOGGLE ENTRE HIDE AND SHOW EN CLIK SUR LE MEME BOUTON
-// $("#aFaire").click(function() {
-// 	$("#doneShow").toggle("slow")
+// // TOGGLE ENTRE HIDE AND SHOW EN CLIK SUR LE MEME BOUTON
+// // $("#aFaire").click(function() {
+// // 	$("#doneShow").toggle("slow")
+// // });
+
+// $(".aFaire").click(function() {
+// 	$(".doneShow").toggle();
+
+
 // });
 
-$(".aFaire").click(function() {
-	$(".doneShow").toggle();
+
+// $(".fini").click(function(){
+// 	$(".toDoShow").toggle();
+// })
+
+// $(".tout").click(function(){
+// 	$(".doneShow, .toDoShow").show();
+// });
+
+// // $(".envoi").change(function(){
+// // $(".tache1").val($("#ajoutTache"));
+// //     });
 
 
-});
 
 
-$(".fini").click(function(){
-	$(".toDoShow").toggle();
+// $("#envoi").click(function(){
+// 	var attr = $("#ajoutTache").val();
+
+
+// 	$("#tache1").html($(attr));
+// });
+
+
+
+
+//     
+
+
+var toDo = ["ticket1", "ticket2"]
+var done = ["ticket3"]
+$("#toDo").html(toDo)
+
+$("#envoi").click(function(){
+	var attr = $("#ajoutTache").val();
+	toDo.push(attr);
+	$("#toDo").html(toDo);
+
 })
-
-$(".tout").click(function(){
-	$(".doneShow, .toDoShow").show();
-});
+$("#fini").click(function(){
+console.log(toDo)})
